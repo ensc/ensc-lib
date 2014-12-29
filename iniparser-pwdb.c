b5b75acf22b6c72e5e0479ae4fa0bad8d3cf6531
Enrico Scholz <ensc@ensc.de>
2014-12-29 20:31:33 +0100

    iniparser: added functions to read uid/gid options

---
+++ iniparser-pwdb.c
@@ -0,0 +1,98 @@
+/*	--*- c -*--
+ * Copyright (C) 2014 Enrico Scholz <enrico.scholz@ensc.de>
+ @PROJECT_LICENSE@
+ */
+
+#include "iniparser.h"
+
+#include <stdbool.h>
+#include <errno.h>
+#include <ctype.h>
+#include <string.h>
+#include <limits.h>
+
+#include <pwd.h>
+#include <grp.h>
+
+#include <et/com_err.h>
+
+#include <iniparser.h>
+
+#include "i18n.h"
+#include "compiler.h"
+#include "logging.h"
+
+static bool is_number(unsigned long *v, char const *str)
+{
+	char		*err;
+
+	errno = 0;
+	*v = strtoul(str, &err, 10);
+
+	return !((*v == ULONG_MAX && errno == ERANGE) ||
+		 (*err && !isspace(*err)));
+}
+
+bool _hidden_ iniparser_getuser(struct _dictionary_ *dict,
+				uid_t *uid, gid_t *gid,
+				char const *key, bool is_critical)
+{
+	char const	*tmp = iniparser_getstring(dict, key, NULL);
+	unsigned long	res;
+
+	if (!tmp)
+		return !is_critical;
+
+	if (strcmp(tmp, "-1") == 0) {
+		res = -1;
+	} else if (!is_number(&res, tmp)) {
+		struct passwd	*pw;
+
+		pw = getpwnam(tmp);
+		if (!pw) {
+			com_err(__func__, errno,
+				N_("failed to convert '%s' (--> '%s') to user\n"),
+				key, tmp);
+			return false;
+		}
+
+		res = pw->pw_uid;
+
+		if (gid)
+			*gid = pw->pw_gid;
+	}
+
+	*uid = res;
+
+	return true;
+}
+
+bool iniparser_getgroup(struct _dictionary_ *dict, gid_t *gid,
+			char const *key, bool is_critical)
+{
+	char const	*tmp = iniparser_getstring(dict, key, NULL);
+	unsigned long	res;
+
+	if (!tmp)
+		return !is_critical;
+
+	if (strcmp(tmp, "-1") == 0) {
+		res = -1;
+	} else if (!is_number(&res, tmp)) {
+		struct group	*grp;
+
+		grp = getgrnam(tmp);
+		if (!grp) {
+			com_err(__func__, errno,
+				N_("failed to convert '%s' (--> '%s') to group\n"),
+				key, tmp);
+			return false;
+		}
+
+		res = grp->gr_gid;
+	}
+
+	*gid = res;
+
+	return true;
+}
--- iniparser.h
+++ iniparser.h
@@ -6,9 +6,18 @@
 #ifndef H_ENSC_LIB_INIPARSER_H
 #define H_ENSC_LIB_INIPARSER_H
 
+#include <stdbool.h>
+#include <sys/types.h>
+
 struct _dictionary_;
 char const *iniparser_getstring_exp(struct _dictionary_ *cfg,
 				    char const *key,
 				    char const *dflt);
+
+bool iniparser_getuser(struct _dictionary_ *cfg, uid_t *uid, gid_t *gid,
+		       char const *key, bool is_critical);
+
+bool iniparser_getgroup(struct _dictionary_ *cfg, gid_t *gid,
+			char const *key, bool is_critical);
 
 #endif	/* H_ENSC_LIB_INIPARSER_H */
