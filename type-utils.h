ce4c21dae3c0196a169eef5f4465a07d447c55ba
Enrico Scholz <enrico.scholz@ensc.de>
2016-08-14 19:16:10 +0200

    type-utils: fixed LICENSE
    Signed-off-by: Enrico Scholz <enrico.scholz@ensc.de>

--- type-utils.h
+++ type-utils.h
@@ -1,17 +1,6 @@
 /*	--*- c -*--
  * Copyright (C) 2016 Enrico Scholz <enrico.scholz@ensc.de>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ @PROJECT_LICENSE@
  */
 
 #ifndef H_ENSC_TYPE_UTILS_H
