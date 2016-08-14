6b5e96c69d6ba80bd7d7ed93e917dfbc5f1db563
Enrico Scholz <enrico.scholz@ensc.de>
2016-08-14 19:00:25 +0200

    unserialize: added testsuite
    Signed-off-by: Enrico Scholz <enrico.scholz@ensc.de>

---
+++ testsuite/unserialize.c
@@ -0,0 +1,26 @@
+/*	--*- c -*--
+ * Copyright (C) 2016 Enrico Scholz <enrico.scholz@ensc.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define UNSERIALIZE_SUPPORT_MEMORY	1
+#define UNSERIALIZE_SUPPORT_SOCKET	1
+#define UNSERIALIZE_SUPPORT_FD		1
+#define UNSERIALIZE_SUPPORT_GENERIC	1
+
+#include "../unserialize.h"
+
+int main(void)
+{
+}
